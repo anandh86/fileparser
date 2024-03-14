90f177aedaee191584383c2e9be00ae6978a4a37
anandhmaps <anandh.maps@gmail.com>
2024-03-14 10:48:43 +0800

    Added support for json files

---
+++ src/CodingAssignmentLib/JsonContentParser.cs
@@ -0,0 +1,23 @@
+﻿using CodingAssignmentLib.Abstractions;
+using Newtonsoft.Json.Linq;
+
+namespace CodingAssignmentLib;
+
+public class JsonContentParser : IContentParser
+{
+    public IEnumerable<Data> Parse(string content)
+    {
+        // Parse JSON string
+        JArray jsonArray = JArray.Parse(content);
+
+        List<Data> dataList = new List<Data>();
+        foreach (JObject item in jsonArray)
+        {
+            string key = item["Key"].ToString();
+            string value = item["Value"].ToString();
+            dataList.Add(new Data(key, value));
+        }
+
+        return dataList;
+    }
+}
