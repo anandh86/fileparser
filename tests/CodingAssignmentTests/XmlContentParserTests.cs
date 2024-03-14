ff0e561b23f3329744e8475544e8cdd053732935
anandhmaps <anandh.maps@gmail.com>
2024-03-14 12:05:22 +0800

    Added test for Xml parser

---
+++ tests/CodingAssignmentTests/XmlContentParserTests.cs
@@ -0,0 +1,50 @@
+﻿using System.Text;
+using CodingAssignmentLib;
+using CodingAssignmentLib.Abstractions;
+
+namespace CodingAssignmentTests;
+
+public class XmlContentParserTests
+{
+    private XmlContentParser _sut = null!;
+
+    [SetUp]
+    public void Setup()
+    {
+        _sut = new XmlContentParser();
+    }
+
+    [Test]
+    public void Parse_ReturnsData()
+    {
+        // Arrange
+        string xmlContent = @"
+            <Datas>
+                <Data>
+                    <Key>YvXYLQtn7V</Key>
+                    <Value>CTDhy3riwv</Value>
+                </Data>
+                <Data>
+                    <Key>aUlIcPeSWo</Key>
+                    <Value>z41HQfrXIL</Value>
+                </Data>
+                <Data>
+                    <Key>pXuhB1R9Lz</Key>
+                    <Value>mkU7lg9xJT</Value>
+                </Data>                
+            </Datas>";
+
+        // Act
+        IEnumerable<Data> result = _sut.Parse(xmlContent);
+
+        // Assert
+        List<Data> expectedData = new List<Data>
+        {
+            new Data("YvXYLQtn7V", "CTDhy3riwv"),
+            new Data("aUlIcPeSWo", "z41HQfrXIL"),
+            new Data("pXuhB1R9Lz", "mkU7lg9xJT")
+        };
+
+        CollectionAssert.AreEqual(expectedData, result);
+    }
+}
