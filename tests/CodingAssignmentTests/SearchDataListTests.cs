aec6dace9028d5237fd35ae4c58fa8dfb80a9c49
anandhmaps <anandh.maps@gmail.com>
2024-03-15 17:05:39 +0800

    Updated unit tests for search feature.

---
+++ tests/CodingAssignmentTests/SearchDataListTests.cs
@@ -0,0 +1,67 @@
+﻿using System.Text;
+using CodingAssignmentLib;
+using CodingAssignmentLib.Abstractions;
+
+namespace CodingAssignmentTests;
+
+public class SearchDataListTests
+{
+    private SearchDataList _sdl = null!;
+
+    [SetUp]
+    public void Setup()
+    {
+        _sdl = new SearchDataList();
+    }
+
+    [Test]
+    public void Search_ReturnsData()
+    {
+        // Arrange
+        List<Data> inputData = new List<Data>
+        {
+            new Data("key1","value1"),
+            new Data("kEy1","value2"),
+            new Data("key2","value3"),
+            new Data("KEY1","value4"),
+            new Data("key3","value5")
+        };
+
+        List<Data> expectedData = new List<Data>
+        {
+            new Data("key1","value1"),
+            new Data("kEy1","value2"),
+            new Data("KEY1","value4")
+        };
+
+        // Act
+        var result = _sdl.Search("key1", inputData);
+
+        // Assert
+        Assert.IsNotEmpty(result);
+        Assert.That(expectedData, Is.EqualTo(result));
+    }
+
+    [Test]
+    public void Search_ReturnsEmpty()
+    {
+        // Arrange
+        List<Data> inputData = new List<Data>
+        {
+            new Data("key1","value1"),
+            new Data("kEy1","value2"),
+            new Data("key2","value3"),
+            new Data("KEY1","value4"),
+            new Data("key3","value5")
+        };        
+
+        // Act
+        var result = _sdl.Search("key4", inputData);
+
+        // Assert
+        Assert.IsEmpty(result);
+    }
+
+
+}
+
